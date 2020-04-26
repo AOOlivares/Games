0d39e20fa60cbb5a28deae46ea3a96196dfaf6f7
Alberto de la Ossa Olivares <alberto.oo@outlook.com>
2020-04-26 12:15:59 +0100

    Updates

---
+++ precache-manifest.0ef5ec1582e9088b7ba99be5c4ee1ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb2e2bb2053c64393a49cf0efaee2b3",
+    "url": "/Nonogram/index.html"
+  },
+  {
+    "revision": "e0b005f1a9e5fdb0ee07",
+    "url": "/Nonogram/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed50c376bd2950f7dc4a",
+    "url": "/Nonogram/static/js/2.2056ba00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nonogram/static/js/2.2056ba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b005f1a9e5fdb0ee07",
+    "url": "/Nonogram/static/js/main.40c541a8.chunk.js"
+  },
+  {
+    "revision": "59eb052f09e367328783",
+    "url": "/Nonogram/static/js/runtime-main.7b49619b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.641a3a9cc3bdcdebe47a577b485a74e3.js"
+  "/Nonogram/precache-manifest.0ef5ec1582e9088b7ba99be5c4ee1ffe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nonogram/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
