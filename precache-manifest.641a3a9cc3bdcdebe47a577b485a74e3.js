5aaa313e9849eb2c76136b2ad941d3d2a5292454
Alberto de la Ossa Olivares <alberto.oo@outlook.com>
2020-04-26 11:54:34 +0100

    enable it

---
+++ precache-manifest.641a3a9cc3bdcdebe47a577b485a74e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e135754db0c78e53047ce293f962f460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c0e1ff359b1910e3b9f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed50c376bd2950f7dc4a",
+    "url": "/static/js/2.2056ba00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2056ba00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0e1ff359b1910e3b9f",
+    "url": "/static/js/main.44dbc22f.chunk.js"
+  },
+  {
+    "revision": "8d27f877f332313429d4",
+    "url": "/static/js/runtime-main.88da3dcc.js"
+  }
+]);
