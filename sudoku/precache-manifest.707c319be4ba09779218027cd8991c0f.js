728fe0fcb2cb49ae4c7a56e0f594c3dabc5ae215
Alberto de la Ossa Olivares <alberto.oo@outlook.com>
2020-05-03 17:30:49 +0100

    Updates

---
+++ sudoku/precache-manifest.707c319be4ba09779218027cd8991c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d432dbce9421e80d65d95b3da32d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f7e7a81ec5bb351e45",
+    "url": "/static/css/main.a3c800ab.chunk.css"
+  },
+  {
+    "revision": "3e8a44e224dca0f22571",
+    "url": "/static/js/2.6304ab4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6304ab4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f7e7a81ec5bb351e45",
+    "url": "/static/js/main.fffb44fe.chunk.js"
+  },
+  {
+    "revision": "5a8f758bb606e76071a2",
+    "url": "/static/js/runtime-main.c6383a69.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
