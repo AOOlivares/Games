e8aa021a351fdecb60b6b5ef0298deffae1de348
Alberto de la Ossa Olivares <alberto.oo@outlook.com>
2020-05-03 16:33:57 +0100

    Updates

--- nonogram/precache-manifest.9ca1ad33181e882b980dc8f30da5bc44.js
+++ nonogram/precache-manifest.9ca1ad33181e882b980dc8f30da5bc44.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "be2c9ad4b589a2885ae18ca6505d78c9",
-    "url": "/nonogram/index.html"
+    "url": "/Games/nonogram/index.html"
   },
   {
     "revision": "986e36d37d8d2153d374",
-    "url": "/nonogram/static/css/main.5ecd60fb.chunk.css"
+    "url": "/Games/nonogram/static/css/main.5ecd60fb.chunk.css"
   },
   {
     "revision": "4c17a6c5ce830b1237e1",
-    "url": "/nonogram/static/js/2.dc8e3c92.chunk.js"
+    "url": "/Games/nonogram/static/js/2.dc8e3c92.chunk.js"
   },
   {
     "revision": "a1a636632721a402612df3e043dd630d",
-    "url": "/nonogram/static/js/2.dc8e3c92.chunk.js.LICENSE.txt"
+    "url": "/Games/nonogram/static/js/2.dc8e3c92.chunk.js.LICENSE.txt"
   },
   {
     "revision": "986e36d37d8d2153d374",
-    "url": "/nonogram/static/js/main.c32d10c8.chunk.js"
+    "url": "/Games/nonogram/static/js/main.c32d10c8.chunk.js"
   },
   {
     "revision": "59eb052f09e367328783",
-    "url": "/nonogram/static/js/runtime-main.7b49619b.js"
+    "url": "/Games/nonogram/static/js/runtime-main.7b49619b.js"
   }
 ]);
--- nonogram/service-worker.js
+++ nonogram/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nonogram/precache-manifest.9ca1ad33181e882b980dc8f30da5bc44.js"
+  "/Games/nonogram/precache-manifest.9ca1ad33181e882b980dc8f30da5bc44.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nonogram/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Games/nonogram/index.html"), {
 
   blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
 });
